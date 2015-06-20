e765d582b157ead1553ff6599bddd235c0af64ec
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-06-20 03:36:10 +0200

    Update helper script

--- scripts/gh-pages-from-staging.sh
+++ scripts/gh-pages-from-staging.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+set -ev
 
+for i in staging/*.html staging/*.js; do
+  cp "$i" .
+  git add "$(basename "$i")"
+done
 
+for i in staging/css/*; do
+  cp "$i" css
+  git add "css/$(basename "$i")"
+done
